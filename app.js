6ca61baeb13edf23e13c1487725f1eee7e71fa39
Phillip Schmidt <pjschmidt3@outlook.com>
2021-12-06 14:42:34 -0600

    completed exercise

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function printOdds (count) {
+  if (count < 0) {
+    console.error('printOdds expects a positive integer parameter');
+    return;
+  }
+
+  for (let i = 0; i < count; i++) {
+    if (i % 2 === 1) {
+      console.log(i);
+    }
+  }
+}
+
+console.log(parseInt(undefined));
+printOdds(100);
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+function checkAge (age) {
+  age = parseInt(age);
+  if (Number.isNaN(age)) {
+    console.error('Invalid integer passed to checkAge');
+    return;
+  }
+
+  const aboveSixteen = 'You can drive!';
+  const belowSixteen = 'Sorry, but you need to wait until you\'re 16.';
+
+  let result;
+  if (age >= 16) {
+    result = aboveSixteen;
+  } else {
+    result = belowSixteen;
+  }
+
+  console.log(result);
+}
+
+console.log('Should be allowed to drive: ');
+checkAge(16);
+
+console.log('Should not be allowed to drive: ');
+checkAge(15);
